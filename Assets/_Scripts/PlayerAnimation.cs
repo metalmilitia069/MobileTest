f52a3b6b5e07869e8a03af01ccaa8ae0a2074247
leonardo medina <medina.engineering@outlook.com>
2021-03-11 02:16:53 -0300

    Player Attack Animation

---
+++ Assets/_Scripts/PlayerAnimation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    private Animator _swordArc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+        _swordArc = transform.GetChild(1).GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(float move)
+    {
+        _anim.SetFloat("Move", Mathf.Abs(move));
+    }
+
+    public void JumpAnimation(bool isJumping)
+    {
+        _anim.SetBool("Jumping", isJumping);
+        Debug.Log("Jumping? " + isJumping);
+    }
+
+    public void AttackAnimation()
+    {
+        _anim.SetTrigger("BasicAttack");
+        _swordArc.SetTrigger("SwordAnimation");
+    }
+}
