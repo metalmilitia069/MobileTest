00a63e3709872c5aba46331781e18942139f8567
leonardo medina <medina.engineering@outlook.com>
2021-03-15 16:46:47 -0300

    Ads Implementation is Depricated

--- Assets/_Scripts/Managers/AdsManager.cs
+++ Assets/_Scripts/Managers/AdsManager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
-public class AdsManager : MonoBehaviour
+public class AdsManager : MonoBehaviour, IUnityAdsListener
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnUnityAdsDidError(string message)
     {
-        
+        throw new System.NotImplementedException();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ShowRewardedAdd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions
+            {
+                //resultCallback = HandleShowResult
+                //Advertisement.AddListener()
+            };
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    void HandleShowResult(ShowResult showResult)
+    {
+        switch (showResult)
+        {
+            case ShowResult.Failed:
+                Debug.Log("The Video Failed");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("You Skipped the Ad Stuff");
+                break;
+            case ShowResult.Finished:
+                //mozo
+                break;
+            default:
+                break;
+        }
     }
 }
